041303b24111997c73ba85c9608109e66779f78c
binit <binit@escapadetechnologies.com>
2019-04-03 13:08:49 +0530

    From Hyderabad

---
+++ CheckingDataReader/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CheckingDataReader
+{
+    class Program
+    {
+        static void CheckMultiResults()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("MultiResult",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            IDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["ShopName"].ToString());
+            }
+
+            if(reader.NextResult())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["CategoryName"].ToString());
+                }
+                
+            }
+
+            con.Close();
+            Console.ReadLine();
+        }
+        
+        static void Main(string[] args)
+        {
+            CheckMultiResults();
+        }
+    }
+}
